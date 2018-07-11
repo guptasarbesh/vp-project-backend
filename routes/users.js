d51fedb25e7f5ac5dffdd1ebaf582bbff8d3eaac
guptasarbesh <sarveshg821@gmail.com>
2018-07-11 14:43:43 +0530

    user registration

--- routes/users.js
+++ routes/users.js
@@ -1,15 +1,30 @@
 const express = require('express');
 const router = express.Router();
 
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
+const User = require('../models/user');
 //register
-router.get('/register',(req,res,next)=>{
- 
-    res.send('REGISTER');
+router.post('/register',(req,res,next)=>{
+    let newUser = new User ({
+        name: req.body.name,
+        email: req.body.email,
+        username: req.body.username,
+        password: req.body.password
+      });
+
+      User.addUser(newUser, (err, user) => {
+        if(err) {
+          res.json({success: false, msg: 'Failed to register user'});
+        } else {
+          res.json({success: true, msg: 'User registered'});
+        }
+      });
 }
 );
 
 //Authenticate
-router.get('/authenticate',(req,res,next)=>{
+router.post('/authenticate',(req,res,next)=>{
     res.send('AUTHENTICATE');
 }
 );
@@ -20,9 +35,5 @@ router.get('/profile',(req,res,next)=>{
     res.send('Profile');
 }
 );
-//validate
-router.get('/validate',(req,res,next)=>{
-    res.send('Validate');
-}
-);
+
 module.exports=router;
